11cecfbcd709ffa26bb8b71560209dd7865ec1ed
Martin Czygan <martin.czygan@gmail.com>
2021-06-13 12:51:21 +0200

    update docs

--- di/basic/main.go
+++ di/basic/main.go
@@ -2,7 +2,8 @@
 
 // Note: The build tag here must come before the package declaration; cf.
 // https://github.com/google/wire/issues/178#issuecomment-493633100,
-// https://github.com/google/wire/issues/178#issuecomment-543349470.
+// https://github.com/google/wire/issues/178#issuecomment-543349470,
+// https://github.com/google/wire/issues/117#issue-407026505
 package main
 
 import (
