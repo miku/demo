7a66307d93ed131afc1dff5eea0377832cecb326
Martin Czygan <martin.czygan@gmail.com>
2021-06-17 18:31:26 +0200

    todo: poly

---
+++ poly/message.go
@@ -0,0 +1,8 @@
+package poly
+
+// TODO: different messages plus an envelop or wrapper with extra information
+// and a pointer to the message. Use interface first.
+// https://making.pusher.com/alternatives-to-sum-types-in-go/
+//
+// Have []byte (json), want static fields, and a dynamic field, depending on
+// some other fields (e.g. the type of message).
