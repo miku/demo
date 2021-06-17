3a513433577e091ca9efb786ece14498169ac8ec
Martin Czygan <martin.czygan@gmail.com>
2021-06-17 18:46:37 +0200

    update notes

--- poly/message.go
+++ poly/message.go
@@ -1,4 +1,9 @@
-package poly
+package main
+
+import (
+	"encoding/json"
+	"time"
+)
 
 // TODO: different messages plus an envelop or wrapper with extra information
 // and a pointer to the message. Use interface first.
@@ -7,3 +12,22 @@ package poly
 //
 // Have []byte (json), want static fields, and a dynamic field, depending on
 // some other fields (e.g. the type of message).
+
+type Message struct {
+	MID  string
+	PID  string
+	T    time.Time
+	Type int
+	Data interface{}
+}
+
+func (m *Message) FromBytes(p []byte, t interface{}) error {
+	m.Data = t
+	return json.Unmarshal(p, m)
+}
+
+func main() {
+	example = `{"mid":"1","pid":"2","t":"123","type":"1","data":{"a":"1","b":"2"}}`
+	msg := Message{}
+	// ...
+}
