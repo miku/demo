8b0aa7c7008794a9e03911a02dfedd715452d349
Martin Czygan <martin.czygan@gmail.com>
2021-06-17 18:36:24 +0200

    add link

--- poly/message.go
+++ poly/message.go
@@ -3,6 +3,7 @@ package poly
 // TODO: different messages plus an envelop or wrapper with extra information
 // and a pointer to the message. Use interface first.
 // https://making.pusher.com/alternatives-to-sum-types-in-go/
+// https://eli.thegreenplace.net/2018/go-and-algebraic-data-types/
 //
 // Have []byte (json), want static fields, and a dynamic field, depending on
 // some other fields (e.g. the type of message).
